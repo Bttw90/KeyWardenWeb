4d4478ccf50d77ffca229868aeb35198c58a67b8
unknown <emanueledefalco90@gmail.com>
2023-09-10 21:29:41 +0200

    Added temporary dictionary and class to test password withdraw

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm, LoginForm
 import secrets
+# Temp import
+from flask_wtf import FlaskForm
+from wtforms import SelectField
 
 app = Flask(__name__)
 
 # Secret key will protect against modify cookies and cross-site request forgery attacks
 foo = secrets.token_urlsafe(16)
 app.secret_key = foo
+
+# Temp dict for testing purpose
+data_dict = {
+    'key1': 'Password1',
+    'key2': 'Password2',
+    'key3': 'Password3',
+}
 
 @app.route('/', methods = ['GET', 'POST'])
 def login():
@@ -21,9 +31,20 @@ def register():
         return redirect(url_for('vault'))
     return render_template('register.html', form = form)
 
-@app.route('/vault')
+# Temp class
+class SelectKeyForm(FlaskForm):
+    select_key = SelectField('Seleziona una chiave', choices=[key for key in data_dict.keys()])
+
+@app.route('/vault', methods = ['GET', 'POST'])
 def vault():
-    return render_template('vault.html')
+    form = SelectKeyForm(data_dict = data_dict)
+    selected_value = None
+
+    if form.validate_on_submit():
+        selected_key = form.select_key.data
+        selected_value = data_dict.get(selected_key, 'Data not found')
+
+    return render_template('vault.html', form = form, selected_value = selected_value)
 
 if __name__ == '__main__':
     app.run()
--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
+from wtforms.validators import DataRequired, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
